dbfa3d114688c5c1347a1ae0648718dc448e60ae
Dhruvil Dave <dhruvildave22@gmail.com>
2021-04-15 12:14:54 +0530

    Setup email functionality for sign-up

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
-# Load the Rails application.
-require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net', 
+  :port => '587', 
+  :authentication => :plain, 
+  :user_name => "photo app api key", 
+  :password => "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", 
+  :domain => 'mailinator.com', 
+  :enable_starttls_auto => true 
+}
